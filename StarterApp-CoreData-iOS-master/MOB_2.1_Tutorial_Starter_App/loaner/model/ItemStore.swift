e336f1f5d12c4c4319d3cf2ba27b21e0e7348ae3
SarinSwift <sarinyaswift@gmail.com>
2019-05-08 18:24:48 -0700

    added persistent container, core data stack, and instantiated in appDelegate

---
+++ StarterApp-CoreData-iOS-master/MOB_2.1_Tutorial_Starter_App/loaner/model/ItemStore.swift
@@ -0,0 +1,24 @@
+//
+//  ItemStore.swift
+//  loaner
+//
+//  Created by Sarin Swift on 5/8/19.
+//  Copyright © 2019 LinnierGames. All rights reserved.
+//  Or in other words, the Core Data Stack
+
+import UIKit
+import CoreData
+
+class ItemStore: NSObject {
+    let persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "LoanedItems")
+        
+        container.loadPersistentStores(completionHandler: { (description, error) in
+            if let error = error {
+                print("Error setting up Core Data: \(error)")
+            }
+        })
+        
+        return container
+    }()
+}
